8ef08c984a84f50b73e09d31f009c1439dee80da
Nuhito Atmoko <atmokonuhito@gmail.com>
2024-09-20 09:07:29 +0700

    perbaikan -ito

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import apps from './apps/apps'; /* Ini nanti bisa diganti ke apa aja */
-import MenuPage from './MenuPage'; /* ini juga */
+import Apps from "./routes/Makanan";
+import MenuAwal from "./routes/MenuAwal";
+ /* Ini nanti bisa diganti ke apa aja */
+// import MenuPage from './MenuPage'; /* ini juga */
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<apps />} />
+        <Route path="/" element={<MenuAwal />} />
         <Route path="/menu" element={<MenuPage />} />
+        <Route path = "/Makanan" element={<Apps/>}></Route>
       </Routes>
     </Router>
   );
 }
 
 export default App;
-};
 
 
---
+++ src/routes/MenuAwal.jsx
@@ -0,0 +1,9 @@
+import Button from "../layouts/Button"
+
+export default function MenuAwal(){
+    return(
+        <main>
+            <Button/>
+        </main>
+    )
+}
